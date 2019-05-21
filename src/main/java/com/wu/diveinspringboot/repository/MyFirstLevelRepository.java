4fa630626e1a6577e2bc77aba1ff4368348dbfd7
wxq8783 <wxq_8783@126.com>
2019-05-21 22:45:53 +0800

    提交first

--- src/main/java/com/wu/diveinspringboot/repository/MyFirstLevelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wu.diveinspringboot.repository;
-
-import com.wu.diveinspringboot.annotation.FirstLevelRepository;
-
-@FirstLevelRepository(value = "myFirstLevelRepository")
-public class MyFirstLevelRepository {
-}
--- src/main/java/com/wu/diveinspringboot/service/CalculateService.java
+++
@@ -1,5 +0,0 @@
-package com.wu.diveinspringboot.service;
-
-public interface CalculateService {
-    Integer sum(Integer... values);
-}
